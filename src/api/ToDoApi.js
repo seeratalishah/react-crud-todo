5be11a6985b1a927289d6c3243e199f34bd6b2a4
seeratalishah <seeratalyshah@gmail.com>
2022-09-24 11:04:55 +0500

    created async function

---
+++ src/api/ToDoApi.js
@@ -0,0 +1,10 @@
+export async function getAllToDos(){
+  const response = await  fetch('http://ec2-50-17-231-251.compute-1.amazonaws.com:4000/api/todoapp');
+  if(response.ok){
+    const jsonResponse = await response.json();
+    return jsonResponse
+  }
+  
+  throw new Error(response);
+  
+}
